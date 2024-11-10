cb2130faab3e56dacad336abf392c303b7704413
Watson Albert <wr971024@gmail.com>
2024-11-10 11:41:34 +0900

    feat: first step as beginner in java

---
+++ Prog1.java
@@ -0,0 +1,12 @@
+package java_for_beginner;
+public class Prog1 {
+    public static void main(String[] args) {
+        System.out.println("\n Prog.is showing even no");
+
+        for (int i = 2; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.print("\n " + i);
+            }
+        }
+    }
+}
